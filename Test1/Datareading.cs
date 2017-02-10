cd6eb4e036c627b0b145bc5327f1497509e7096d
Johny17 <tamyhel2@gmail.com>
2017-02-10 18:03:52 +0330

    First commit

---
+++ Test1/Datareading.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+using System.Web;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace Test1
+{
+    public partial class Datareading : Form
+    {
+        public Datareading()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var webclient = new WebClient();
+            var pageSourceCode = webclient.DownloadString("http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=" + textBox1.Text + "&redirects=true");
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(pageSourceCode);
+
+            var fmode = doc.GetElementsByTagName("extract")[0];
+
+
+            try
+            {
+                string ss = fmode.InnerText;
+                Regex regex = new Regex("<[^>]*>");
+                string.Format("Before:{0}", ss);
+                ss = regex.Replace(ss, string.Empty);
+                string result = string.Format(ss);
+                richTextBox1.Text += result;
+                
+            
+            }
+            catch (Exception)
+            {
+                richTextBox1.Text = "error";
+            }
+
+        }
+
+    }
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+
+namespace Test1
+{
+    static class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            //RecognitionCore core = new RecognitionCore();
+
+            //SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            //CommandBuilder cmdBuilder = new CommandBuilder();
+
+            //ChoiceSet choices = new ChoiceSet();
+
+            //choices
+            //        .Add("google");
+            //        .Add("youtube");
+
+            //cmdBuilder.Text("search")
+            //    .choices("search_provider", choices)
+            //    .Text("for")
+            //    .Any()
+            //    .Handle(context =>
+            //    {
+            //    var words = context.Phrase.Words.Skip(3));
+
+            //    switch (choices Value)
+            //        {
+            //    case "youtube":
+            //    searchgoogle(searchTerm);
+            //    break;
+            //}
+
+
+           
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Datareading());
+
+        }
+
+        private static void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
